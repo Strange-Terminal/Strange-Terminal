4dae3e5545ebd8a9d19a0c95347caf0b9395d877
Strange-Terminal <110657792+Strange-Terminal@users.noreply.github.com>
2022-08-05 10:04:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os,platform
+print("Hello in Strange Terminal")
+keywords=["CW","CWV"]
+while True:
+    command=input(str(os.path.abspath(os.getcwd()))+">>")
+    if "CW" in command:
+            print(command[command.find('"')+1:-1])  
+    elif "CWV" in command :
+        print(globals()[str(command[3:]).replace(" ","")])   
+    if "cw" in command:
+            print(command[command.find('"')+1:-1])  
+    elif "cwv" in command :
+        print(globals()[str(command[3:]).replace(" ","")])  
+    elif "=" in command :
+       globals()[str(command[0])] = command[2:]
+    elif "clear" in command:
+        if platform.system() ==  "Linux":
+            os.system("clear")
+        elif platform.system() == "Windows":
+            os.system("cls")
